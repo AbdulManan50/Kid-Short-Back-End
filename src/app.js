17910cdb3dbd614162161e9bf15cc7f9588450cb
Abdul Manan <abdul.manan@novatoresols.com>
2026-03-11 13:03:14 +0500

    app.js

--- src/app.js
+++ src/app.js
@@ -5,10 +5,19 @@ const cors = require("cors");
 
 const authRoutes = require("./routes/auth.routes");
 const videoRoutes = require("./routes/videoRoutes");
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+// Root route
+app.get("/", (req, res) => {
+  res.json({
+    status: "API Running 🚀",
+    message: "Kids Short Video Backend",
+  });
+});
 
 // Routes
 app.use("/api/auth", authRoutes);
